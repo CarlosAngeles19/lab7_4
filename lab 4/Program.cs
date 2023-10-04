df76bbfc44fb532066de6478bb3b6c85cfb7e9af
CarlosAngeles19 <carlosangeles.asc@gmail.com>
2023-10-03 19:01:24 -0500

    Agregar archivos de proyecto.

---
+++ lab 4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("Ingresa un número: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int j = 0; j < n; j++)
+            {
+                for (int k = 0; k < n * 2; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
